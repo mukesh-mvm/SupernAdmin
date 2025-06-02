a49cd1ab15611c712a8c7ca5800f46c970b4760d
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-06-02 18:02:29 +0530

    first commit

---
+++ src/helper/Helper.jsx
@@ -0,0 +1,7 @@
+// const baseurl = "http://localhost:5001";
+
+// const baseurl = "https://jobpakki-backend.onrender.com";
+// const baseurl = "https://api.leadershipladder.in";
+const baseurl = "https://api.supernpro.com";
+// const baseurl = "http://82.25.109.68:5001";
+export { baseurl };
